baa7bfa8d8db8036cb494f78020463ab78075d6d
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-04-19 17:10:07 -0400

    woo hoo!

--- assets/javascript/character.js
+++ assets/javascript/character.js
@@ -6,8 +6,12 @@
       roll: "none",
       health: 0,
       attack: 0,
-      counter: 0,
-      size: 150
+      counter: 0
+    },
+
+    _init: function() {
+      this.element.data("health", this.options.health);
+      this.element.data("attack", this.options.attack);
     },
 
     name: function() {
@@ -41,19 +45,30 @@
       }
 
       this.options.attack = parseInt(value);
+      this._update();
     },
 
     counter: function() {
       return this.options.counter;
     },
 
-    size: function(value) {
-      if (value === undefined) {
-        return this.options.size;
+    hit: function(opponent) {
+      var me = this;
+
+      if (me.health() > 0) {
+        // attack opponent
+        opponent.character("health", opponent.character("health") - me.attack());
+
+        // opponent counter attacks
+        me.health(me.health() - opponent.character("counter"));
+
+        // add experiance points
+        me.attack(me.attack() + this.element.data("attack"));
+
+        return true;
       }
 
-      this.options.size = parseInt(value);
-      this._update();
+      return false;
     },
 
     _create: function() {
@@ -88,13 +103,16 @@
     },
 
     _update: function() {
-      this.imageEl.width(this.options.size);
-      this.healthEl.text(this.options.health);
+      this.imageEl.width(this.options.roll === "none" ? 75 : 150);
+      this.healthEl
+        .css({
+          fontSize: this.options.roll === "none" ? "1em" : "2em"
+        })
+        .text(this.options.health);
       this.element
         .attr("title", [this.options.name, "Attack: " + this.options.attack, "Counter: " + this.options.counter].join("\n"))
-        .removeClass("enemy")
-        .removeClass("player")
-        .addClass(this.options.roll);
+        .removeClass("roll-none roll-enemy roll-player")
+        .addClass("roll-" + this.options.roll);
     }
   });
 })(jQuery);
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,23 @@
       characters: []
     },
 
-    attack: function(player, opponent) {
-      var h = this.characterEls[0].character("health");
-      console.log("health:", h);
-      this.characterEls[0].character("health", h - 10);
-    },
-
-    _characterSelect: function(el) {
-      console.log({ select: el.character("name"), health: el.character("health") });
-    },
+    message: "Select a Player",
 
     _create: function() {
       var game = this;
 
       game.messageEl = $("<div>").addClass("game-message");
-      game.charactersEl = $("<div>").addClass("game-characters clearfix");
+      game.charactersEl = $("<div>").addClass("game-characters");
       game.playerEl = $("<div>").addClass("game-player");
       game.opponentEl = $("<div>").addClass("game-opponent");
-      game.attackButtonEl = $("<button>").text("Attack ->").addClass("btn btn-danger");
+      game.attackButtonEl = $("<button>")
+        .text("Attack ->")
+        .addClass("btn btn-danger")
+        .on("click", function() {
+          game._attack();
+        });
+
+      game.grave = $("<div>");
 
       $.each(game.options.characters, function() {
         var character = this;
@@ -43,15 +42,6 @@
             .append(
               $("<div>")
                 .addClass("col-12")
-                .append(game.messageEl)
-            )
-        )
-        .append(
-          $("<div>")
-            .addClass("row")
-            .append(
-              $("<div>")
-                .addClass("col-12")
                 .append(game.charactersEl)
             )
         )
@@ -60,7 +50,16 @@
             .addClass("row")
             .append(
               $("<div>")
-                .addClass("col-4")
+                .addClass("col-12")
+                .append(game.messageEl)
+            )
+        )
+        .append(
+          $("<div>")
+            .addClass("row")
+            .append(
+              $("<div>")
+                .addClass("col-4 d-flex justify-content-center align-items-center")
                 .append(game.playerEl)
             )
             .append(
@@ -70,7 +69,7 @@
             )
             .append(
               $("<div>")
-                .addClass("col-4")
+                .addClass("col-4 d-flex justify-content-center align-items-center")
                 .append(game.opponentEl)
             )
         )
@@ -79,8 +78,118 @@
       game._update();
     },
 
+    _toJson: function(el) {
+      return {
+        name: el.character("name"),
+        health: el.character("health"),
+        attack: el.character("attack"),
+        counter: el.character("counter")
+      };
+    },
+
+    _player: function() {
+      return this.playerEl.children().first();
+    },
+
+    _opponent: function() {
+      return this.opponentEl.children().first();
+    },
+
+    _attack: function() {
+      var game = this;
+
+      var player = game._player();
+      var opponent = game._opponent();
+
+      var player_name = player.character("name");
+      var opponent_name = opponent.character("name");
+      var player_ap = player.character("attack");
+      var opponent_cp = opponent.character("counter");
+
+      if (player.character("hit", opponent)) {
+        game.message = [
+          player_name + " hits " + opponent_name + " for " + player_ap + " points of damage.",
+          opponent_name + " counters " + player_name + " for " + opponent_cp + " points of damage."
+        ].join("<br/>");
+      }
+
+      var player_health = player.character("health");
+      var opponent_health = opponent.character("health");
+
+      if (player_health > 0 && opponent_health <= 0) {
+        game.grave.append(opponent);
+        game.message += [game.message, '<span class="text-danger">' + opponent_name + " eliminated.</span>", "Select a new Opponent."].join("<br/>");
+      } else if (player_health <= 0) {
+        game._popup(player_name + " eliminated!", "Game Over");
+      }
+
+      game._update();
+    },
+
+    _popup: function(message, title) {
+      $("<div>")
+        .addClass("modal fade")
+        .attr({ role: "dialog" })
+        .append(
+          $("<div>")
+            .addClass("modal-dialog modal-sm")
+            .append(
+              $("<div>")
+                .addClass("modal-content bg-dark")
+                .append(
+                  $("<div>")
+                    .addClass("modal-header")
+                    .append($("<h5>").text(title))
+                    .append(
+                      $("<button>")
+                        .addClass("close text-light")
+                        .attr({ type: "button", "data-dismiss": "modal", "aria-label": "Close" })
+                        .append(
+                          $("<span>")
+                            .attr({ "aria-hidden": true })
+                            .text("x")
+                        )
+                    )
+                )
+                .append(
+                  $("<div>")
+                    .addClass("modal-body")
+                    .append(
+                      $("<h5>")
+                        .addClass(" text-center text-danger")
+                        .text(message)
+                    )
+                )
+            )
+        )
+        .modal({ keyboard: false, backdrop: false });
+    },
+
+    _characterSelect: function(el) {
+      var game = this;
+
+      if (game.playerEl.children().length === 0) {
+        game.playerEl.append(el.character("roll", "player"));
+        game.message = "Select an Opponent";
+      } else if (game.opponentEl.children().length === 0) {
+        game.opponentEl.append(el.character("roll", "enemy"));
+        game.message = "Go!";
+      }
+
+      game._update();
+    },
+
     _update: function() {
-      this.messageEl.text("Select a Player");
+      var game = this;
+
+      game.attackButtonEl.attr("disabled", true);
+      game.messageEl.html(game.message);
+
+      if (game._player().length > 0 && game._opponent().length > 0) {
+        if (game._player().character("health") > 0 && game._opponent().character("health") > 0) {
+          game.attackButtonEl.attr("disabled", false);
+        }
+      }
     }
   });
 })(jQuery);
